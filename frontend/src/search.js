55d992281f2f00553370af0d57f956c8d20b3439
Andrea Gonzales <andreafgonzales@gmail.com>
2019-12-11 23:54:02 -0500

    basic popup

--- frontend/src/search.js
+++ frontend/src/search.js
@@ -9,90 +9,115 @@ async function loadAutocomplete() {
     let station_json = await response.json();
     let stop_data = Object.values(station_json).filter( s => !s.stop_id.toString().match(/[NS]$/) );
 
-    let currentFocus;
+    let focus;
 
     input.addEventListener("input", function(e) {
         let value = this.value;
 
         closeAllLists();
-        currentFocus = -1;
+        focus = -1;
 
         let list = document.createElement("div");
-        list.setAttribute("id", this.id + "autocomplete-list");
-        list.setAttribute("class", "autocomplete-items");
+        list.setAttribute("id", this.id + "ac-list");
+        list.setAttribute("class", "ac-items");
         document.getElementById("results").appendChild(list);
 
         if (!value) return false;
-        var regex = new RegExp(value, "i");
+        let regex = new RegExp(value, "i");
         let matches = stop_data.filter(s => regex.exec(s.stop_name));
         matches.forEach( m => {
             let result = document.createElement("div");
             result.innerHTML += "<input value='" + m.stop_name + "'>";
             result.addEventListener("click", function(e) {
                 input.value = this.getElementsByTagName("input")[0].value;
-                console.log(map);
+                let coordinates = [m.stop_lon, m.stop_lat];
                 map.flyTo({
-                    center: [m.stop_lon, m.stop_lat],
+                    center: coordinates,
                     zoom: 15,
                 });
+
+                let html = `
+                    <div id="popup">
+                        <h3>${m.stop_name}</h3>
+                    </div>
+                `;
+
+                let popup = new mapboxgl.Popup({
+                    closeButton: true,
+                    closeOnClick: true
+                })
+                    .setLngLat(coordinates)
+                    .setHTML(html);
+
+                popup.addTo(map);
                 closeAllLists();
             });
             list.appendChild(result);
         });
     });
 
-    /*execute a function presses a key on the keyboard:*/
     input.addEventListener("keydown", function(e) {
-        var list = document.getElementById(this.id + "autocomplete-list");
+        let list = document.getElementById(this.id + "ac-list");
         if (list) list = list.getElementsByTagName("div");
-        if (e.keyCode == 40) {
-            /*If the arrow DOWN key is pressed,
-            increase the currentFocus variable:*/
-            currentFocus++;
-            /*and and make the current item more visible:*/
+        if (e.keyCode == 40) { //down
+            focus++;
             addActive(list);
         } else if (e.keyCode == 38) { //up
-            /*If the arrow UP key is pressed,
-            decrease the currentFocus variable:*/
-            currentFocus--;
-            /*and and make the current item more visible:*/
+            focus--;
             addActive(list);
-        } else if (e.keyCode == 13) {
-            /*If the ENTER key is pressed, prevent the form from being submitted,*/
+        } else if (e.keyCode == 13) { //enter
             e.preventDefault();
-            if (currentFocus > -1) {
-            /*and simulate a click on the "active" item:*/
-            if (list) list[currentFocus].click();
+            if (focus > -1) {
+            if (list) list[focus].click();
             }
         }
     });
 
     function addActive(list) {
-        /*a function to classify an item as "active":*/
         if (!list) return false;
-        /*start by removing the "active" class on all items:*/
         removeActive(list);
-        if (currentFocus >= list.length) currentFocus = 0;
-        if (currentFocus < 0) currentFocus = (list.length - 1);
-        /*add class "autocomplete-active":*/
-        list[currentFocus].classList.add("autocomplete-active");
+        if (focus >= list.length) focus = 0;
+        if (focus < 0) focus = (list.length - 1);
+        list[focus].classList.add("ac-active");
     }
 
     function removeActive(list) {
-        /*a function to remove the "active" class from all autocomplete items:*/
-        for (var i = 0; i < list.length; i++) {
-            list[i].classList.remove("autocomplete-active");
+        for (let i = 0; i < list.length; i++) {
+            list[i].classList.remove("ac-active");
         }
     }
 
     function closeAllLists(element) {
-        var list = document.getElementsByClassName("autocomplete-items");
-        for (var i = 0; i < list.length; i++) {
+        let list = document.getElementsByClassName("ac-items");
+        for (let i = 0; i < list.length; i++) {
             if (element != list[i] && element != input) {
                 list[i].parentNode.removeChild(list[i]);
             }
         }
     }
+}
+
+const display_date = date => {
+  let diff = ((new Date()) - date) / 1000; // convert ms to sec
+  if( diff < 60 ){
+    return 'now';
+  }
+  diff = Math.round(diff / 60); // convert sec to min
+  if(diff < 60 ){
+    return `${diff}m`;
+  }
+  diff = Math.round(diff / 60); // convert min to hour
+  if( diff < 24 ){
+    return `${diff}h`;
+  }
+  diff = Math.round(diff / 24); // convert hours to days
+  if( diff < 30){
+    return `${diff}d`;
+  }
+  if(diff < 365){ // if within one year, use Month day
+    return yearFormat(date);
+  }
+  return fullFormat(date)
 }
 
 window.addEventListener("load", async function() {
